5f36ac22624f6aafa48105856f6c2b41ad83472d
plumSemPy <emin@berkeley.edu>
2018-01-24 22:31:01 -0800

    write the assembly for darekening and lightening the pixels

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,50 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+  @SCREEN
+  D=A
+  @i
+  M=D
+
+  @KBD  // Read the keyboard address
+  D=M   // Save it's data
+  @DARKEN
+  D;JGT  // If any key is pressed jump to DARKEN
+  (LIGHTENLOOP) // Same logic as DARKEN
+    @KBD
+	D=M
+	@LOOP
+	D;JGT      // If any key is pressed jump back to LOOP
+	@i
+	A=M
+	M=0
+	A=A+1
+	D=A
+	@i
+	M=D
+	@LIGHTENLOOP
+	0;JMP
+    @LOOP
+    0;JMP
+(DARKEN)
+  @SCREEN  // Get the first SCREEN address
+  D=A      // Save it's address
+  @i       // Get a random register
+  M=D      // Put the SCREEN address that is in D in i
+  (DARKENLOOP)
+	@KBD  // Get the keyboard address
+	D=M   // Save it's valus in D
+	@LOOP 
+	D;JEQ // If no key is pressed jump back to LOOP
+	@i    // Get the i register
+	A=M   // Go to the address that i is showing
+	M=-1  // "blacken" that address
+	A=A+1 // Go to the next word
+	D=A   // Save the address in D
+	@i
+	M=D   // Save the address in i
+	@DARKENLOOP
+	0;JMP // Jump to DAREKNLOOP
+    @LOOP
+    0;JMP
