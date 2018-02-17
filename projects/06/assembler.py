39480e4d70845d48a2c9067d047241dece4b402b
plumSemPy <emin@berkeley.edu>
2018-02-17 15:57:25 -0800

    working no symbol assembler

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -1,24 +1,26 @@
+import argparse
+
 class Parser(object):
     def __init__(self, filename):
        self.file_obj = open(filename, 'rb')
+       self.lines = self.file_obj.readlines()
        self.current_command = None
-    def __enter__(self):
-        return self.file_obj
-    def __exit__(self, type, value, traceback):
-        self.file_obj.close()
-    def has_more_commands(self):
-        return None
-    def advance(self):
-        self.current_command = self.file_obj.readline()
-        return self.current_command
+    def set_next_command(self, line):
+        if line and len(line) > 0 :
+            self.current_command = line.strip()
+        return self
     @property
     def command_type(self):
-        if self.current_command[0] == '@':
-            return 'A_COMMAND'
-        elif self.current_command[0] == '(':
-            return 'L_COMMAND'
-        else:
-            return 'C_COMMAND'
+        if len(self.current_command) > 0:
+            if self.current_command[0:2] == '//':
+                return 'COMMENT'
+            elif self.current_command[0] == '@':
+                return 'A_COMMAND'
+            elif self.current_command[0] == '(':
+                return 'L_COMMAND'
+            elif self.current_command.find('=') != -1 or self.current_command.find(';') != -1:
+                return 'C_COMMAND'
+        return 'OTHER'
     def symbol(self):
         if self.command_type == 'A_COMMAND':
             return self.current_command[1:]
@@ -30,19 +32,15 @@ class Parser(object):
         if self.command_type == 'C_COMMAND':
             equal_sign = self.current_command.find('=')
             if not equal_sign == -1:
-                return self.current_comment[:equal_sign]
+                return self.current_command[:equal_sign]
             else:
                 return None
     def comp(self):
         if self.command_type == 'C_COMMAND':
-            beginning = self.current_command.find('=')
-            if beginning == -1:
-                beginning = 0
-            ending = self.current_command.find(';')
-            # extraneous step but helps clarification
+            # even if the first statement returns -1 this will work
+            beginning = self.current_command.find('=') + 1
             # if the ';' was not found, set it to the last char
-            if ending == -1:
-                ending = -1
+            ending = self.current_command.find(';')
             if ending == -1:
                 return self.current_command[beginning:]
             else:
@@ -57,6 +55,7 @@ class Parser(object):
                 return None
 
 class Code(object):
+    @staticmethod
     def dest(mnemonic):
         return_value = ['0', '0', '0']
         if 'M' in mnemonic:
@@ -66,9 +65,40 @@ class Code(object):
         if 'D' in mnemonic:
             return_value[1] = '1'
         return ''.join(return_value)
+    @staticmethod
     def comp(mnemonic):
-        ...
-        return
+        decode = {
+            '0': '0101010',
+            '1': '0111111',
+            '-1': '0111010',
+            'D': '0001100',
+            'A': '0110000',
+            '!D': '0001101',
+            '!A': '0110001',
+            '-D': '0001111',
+            '-A': '0110011',
+            'D+1': '0011111',
+            'A+1': '0110111',
+            'D-1': '0001110',
+            'A-1': '0110010',
+            'D+A': '0000010',
+            'D-A': '0010011',
+            'A-D': '0000111',
+            'D&A': '0000000',
+            'D|A': '0010101',
+            'M': '1110000',
+            '!M': '1110001',
+            '-M': '1110011',
+            'M+1': '1110111',
+            'M-1': '1110010',
+            'D+M': '1000010',
+            'D-M': '1010011',
+            'M-D': '1000111',
+            'D&M': '1000000',
+            'D|M': '1010101'
+        }
+        return decode[mnemonic]
+    @staticmethod
     def jump(mnemonic):
         # no need to be smarter
         decode = {
@@ -82,3 +112,28 @@ class Code(object):
           'JMP': '111'
         }
         return decode[mnemonic]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("address", help="Address of the file to be compiles")
+args = parser.parse_args()
+
+filename_beginning = args.address.rfind('/') + 1
+filename_ending = args.address.find('.asm')
+output_filename = args.address[filename_beginning: filename_ending]
+
+if __name__ == '__main__':
+    output = []
+    p = Parser(args.address)
+    for line in p.lines:
+        p.set_next_command(line)
+        if p.command_type in ['A_COMMAND', 'L_COMMAND']:
+            output.append(p.current_command + '\n')
+        elif p.command_type == 'C_COMMAND':
+            result_to_output = '111'
+            result_to_output += Code.comp(p.comp())
+            result_to_output += Code.dest(p.dest())
+            result_to_output += Code.jump(p.jump())
+            output.append(result_to_output + '\n')
+    p.file_obj.close()
+    with open('{}.hack'.format(output_filename), 'wb') as out:
+        out.writelines(output)
