40d3ef13680b8af2ff46532f851eaa49c06484ef
plumSemPy <emin@berkeley.edu>
2018-04-07 13:14:31 -0700

    basic test assembly

---
+++ projects/07/MemoryAccess/BasicTest/BasicTest.asm
@@ -0,0 +1,474 @@
+
+            //////////
+            // PUSH //
+            //////////
+        
+            @10
+            D=A
+                
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            ///////
+            //POP//
+            ///////
+            
+            @0  // get the offset
+            D=A
+            @LCL  // get the memory
+            D=M+D // save the address
+
+            @R13
+            M=D   // put the address in a general register
+
+            @SP   // get the stack
+            M=M-1 // decreas the stack pointer
+            A=M   // go to that address
+
+            D=M   // save the value
+
+            @R13
+            A=M   // go to the saved address
+            M=D   // put the stack value there
+            
+            //////////
+            // PUSH //
+            //////////
+        
+            @21
+            D=A
+                
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            //////////
+            // PUSH //
+            //////////
+        
+            @22
+            D=A
+                
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            ///////
+            //POP//
+            ///////
+            
+            @2  // get the offset
+            D=A
+            @ARG  // get the memory
+            D=M+D // save the address
+
+            @R13
+            M=D   // put the address in a general register
+
+            @SP   // get the stack
+            M=M-1 // decreas the stack pointer
+            A=M   // go to that address
+
+            D=M   // save the value
+
+            @R13
+            A=M   // go to the saved address
+            M=D   // put the stack value there
+            
+            ///////
+            //POP//
+            ///////
+            
+            @1  // get the offset
+            D=A
+            @ARG  // get the memory
+            D=M+D // save the address
+
+            @R13
+            M=D   // put the address in a general register
+
+            @SP   // get the stack
+            M=M-1 // decreas the stack pointer
+            A=M   // go to that address
+
+            D=M   // save the value
+
+            @R13
+            A=M   // go to the saved address
+            M=D   // put the stack value there
+            
+            //////////
+            // PUSH //
+            //////////
+        
+            @36
+            D=A
+                
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            ///////
+            //POP//
+            ///////
+            
+            @6  // get the offset
+            D=A
+            @THIS  // get the memory
+            D=M+D // save the address
+
+            @R13
+            M=D   // put the address in a general register
+
+            @SP   // get the stack
+            M=M-1 // decreas the stack pointer
+            A=M   // go to that address
+
+            D=M   // save the value
+
+            @R13
+            A=M   // go to the saved address
+            M=D   // put the stack value there
+            
+            //////////
+            // PUSH //
+            //////////
+        
+            @42
+            D=A
+                
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            //////////
+            // PUSH //
+            //////////
+        
+            @45
+            D=A
+                
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            ///////
+            //POP//
+            ///////
+            
+            @5  // get the offset
+            D=A
+            @THAT  // get the memory
+            D=M+D // save the address
+
+            @R13
+            M=D   // put the address in a general register
+
+            @SP   // get the stack
+            M=M-1 // decreas the stack pointer
+            A=M   // go to that address
+
+            D=M   // save the value
+
+            @R13
+            A=M   // go to the saved address
+            M=D   // put the stack value there
+            
+            ///////
+            //POP//
+            ///////
+            
+            @2  // get the offset
+            D=A
+            @THAT  // get the memory
+            D=M+D // save the address
+
+            @R13
+            M=D   // put the address in a general register
+
+            @SP   // get the stack
+            M=M-1 // decreas the stack pointer
+            A=M   // go to that address
+
+            D=M   // save the value
+
+            @R13
+            A=M   // go to the saved address
+            M=D   // put the stack value there
+            
+            //////////
+            // PUSH //
+            //////////
+        
+            @510
+            D=A
+                
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            ///////
+            //POP//
+            ///////
+            
+            @SP   // get the stack
+            M=M-1 // decreas the stack pointer
+            A=M   // go to that address
+
+            D=M   // save the value
+
+            @R11
+            M=D   // put the stack value there
+            
+            //////////
+            // PUSH //
+            //////////
+        
+            @0  // get the offset
+            D=A
+            @LCL  // get the memory
+            A=M+D // go to the index
+            D=M   // get the value
+                
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            //////////
+            // PUSH //
+            //////////
+        
+            @5  // get the offset
+            D=A
+            @THAT  // get the memory
+            A=M+D // go to the index
+            D=M   // get the value
+                
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            ///////
+            //ADD//
+            //////
+            @SP  // get the stack
+            M=M-1 // the current stack position is empty
+            A=M  // go to the top of the stack
+
+            D=M  // save that number
+
+            // I decided not to clean up the stack to zero
+            // cause I don't think that is necessary
+
+            @SP  // decrease the stack pointer
+            M=M-1
+
+            A=M  // go to the next number
+            D=D+M // add
+
+            M=D  // put it back in the stack
+
+            @SP
+            M=M+1
+            
+            //////////
+            // PUSH //
+            //////////
+        
+            @1  // get the offset
+            D=A
+            @ARG  // get the memory
+            A=M+D // go to the index
+            D=M   // get the value
+                
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            ///////
+            //SUB//
+            //////
+            @SP  // get the stack
+            M=M-1 // the current stack position is empty
+            A=M  // go to the top of the stack
+
+            D=-M  // save that number
+
+            // I decided not to clean up the stack to zero
+            // cause I don't think that is necessary
+
+            @SP  // decrease the stack pointer
+            M=M-1
+
+            A=M  // go to the next number
+            D=D+M // add
+
+            M=D  // put it back in the stack
+
+            @SP
+            M=M+1
+            
+            //////////
+            // PUSH //
+            //////////
+        
+            @6  // get the offset
+            D=A
+            @THIS  // get the memory
+            A=M+D // go to the index
+            D=M   // get the value
+                
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            //////////
+            // PUSH //
+            //////////
+        
+            @6  // get the offset
+            D=A
+            @THIS  // get the memory
+            A=M+D // go to the index
+            D=M   // get the value
+                
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            ///////
+            //ADD//
+            //////
+            @SP  // get the stack
+            M=M-1 // the current stack position is empty
+            A=M  // go to the top of the stack
+
+            D=M  // save that number
+
+            // I decided not to clean up the stack to zero
+            // cause I don't think that is necessary
+
+            @SP  // decrease the stack pointer
+            M=M-1
+
+            A=M  // go to the next number
+            D=D+M // add
+
+            M=D  // put it back in the stack
+
+            @SP
+            M=M+1
+            
+            ///////
+            //SUB//
+            //////
+            @SP  // get the stack
+            M=M-1 // the current stack position is empty
+            A=M  // go to the top of the stack
+
+            D=-M  // save that number
+
+            // I decided not to clean up the stack to zero
+            // cause I don't think that is necessary
+
+            @SP  // decrease the stack pointer
+            M=M-1
+
+            A=M  // go to the next number
+            D=D+M // add
+
+            M=D  // put it back in the stack
+
+            @SP
+            M=M+1
+            
+            //////////
+            // PUSH //
+            //////////
+        
+            @R11
+            D=M
+            
+            @SP  // get the stack
+            A=M   // go to that address
+
+            M=D   // save the value
+
+            @SP
+            M=M+1  // increase the stack pointer
+        
+            ///////
+            //ADD//
+            //////
+            @SP  // get the stack
+            M=M-1 // the current stack position is empty
+            A=M  // go to the top of the stack
+
+            D=M  // save that number
+
+            // I decided not to clean up the stack to zero
+            // cause I don't think that is necessary
+
+            @SP  // decrease the stack pointer
+            M=M-1
+
+            A=M  // go to the next number
+            D=D+M // add
+
+            M=D  // put it back in the stack
+
+            @SP
+            M=M+1
+            
+            (INFINITE_LOOP)
+              @INFINITE_LOOP
+              0;JMP
